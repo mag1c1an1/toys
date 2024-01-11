afe30557c8eccd96bb2657ddf3d71294dac75b76
mag1c1an1 <mag1cian@icloud.com>
2024-01-11 13:53:33 +0800

    (feat): add main

--- async/my-redis/src/main.rs
+++ async/my-redis/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("xx");
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    let mut conn = Connection::new(socket);
+    if let Some(frame) = conn.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+        let resp = Frame::Error("unimplemented".to_string());
+        conn.write_frame(&resp).await.unwrap()
+    }
 }
