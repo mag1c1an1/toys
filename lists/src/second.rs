8733eb65460cb5d58b43b7318e6741557005c5cc
mag1c1an1 <mag1cian@icloud.com>
2024-02-01 11:03:57 +0800

    finish second
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -9,17 +9,20 @@ struct Node<T> {
     next: Link<T>,
 }
 struct ListIntoIter<T>(List<T>);
-impl<T> Iterator for ListIntoIter<T> {
-    type Item = T;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.pop()
-    }
+struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
 }
+
+struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         Self { head: None }
     }
+
     pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem,
@@ -27,17 +30,32 @@ impl<T> List<T> {
         });
         self.head = Some(new_node)
     }
+    
     pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|n| {
             self.head = n.next;
             n.elem
         })
     }
+
     pub fn peek(&self) -> Option<&T> {
         self.head.as_ref().map(|n| &n.elem)
     }
+
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|n| &mut n.elem)
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
     }
 }
 
@@ -60,8 +78,40 @@ impl<T> IntoIterator for List<T> {
     }
 }
 
+impl<T> Iterator for ListIntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|n| {
+            self.next = n.next.as_deref();
+            &n.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|n| {
+            self.next = n.next.as_deref_mut();
+            &mut n.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use std::vec;
+
     use super::*;
     #[test]
     fn basics() {
@@ -79,6 +129,7 @@ mod test {
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
     }
+
     #[test]
     fn test_peek() {
         let mut list = List::new();
@@ -95,6 +146,7 @@ mod test {
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
     }
+
     #[test]
     fn test_into_iter() {
         let mut list = List::new();
@@ -107,5 +159,31 @@ mod test {
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
     }
 }
