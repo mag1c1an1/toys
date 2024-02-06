f09a96dc1b3e111146c8f10c354671415a22c0a0
mag1c1an1 <mag1cian@icloud.com>
2024-02-06 13:46:36 +0800

    add playground
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

---
+++ mini-lsm/src/iterators.rs
@@ -0,0 +1,13 @@
+pub trait StorageIterator {
+    type KeyType<'a>: PartialEq + Eq + PartialOrd + Ord
+    where
+        Self: 'a;
+
+    fn value(&self) -> &[u8];
+    fn key(&self) -> Self::KeyType<'_>;
+    fn is_valid(&self) -> bool;
+    fn next(&mut self) -> anyhow::Result<()>;
+    fn num_active_iterators(&self) -> usize {
+        1
+    }
+}
---
+++ mini-lsm/src/mem_table.rs
@@ -0,0 +1,9 @@
+#![allow(dead_code)]
+
+pub struct MemTable {
+  
+}
+
+pub struct MemTableIterator {
+  
+}
